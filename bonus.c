dfd207b06dda48860dcb9b10a8d3830efd03be13
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-05-08 19:53:32 +0900

    [ref] test0

--- bonus.c
+++ bonus.c
@@ -6,7 +6,7 @@
 /*   By: tharaguc <tharaguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 19:33:53 by tharaguc          #+#    #+#             */
-/*   Updated: 2022/05/08 19:50:25 by tharaguc         ###   ########.fr       */
+/*   Updated: 2022/05/08 19:53:06 by tharaguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(void)
 	int	i, j;
 
 	j = 0;
-	fd[0] = open("test.txt", O_RDONLY);
+	fd[0] = open("test0.txt", O_RDONLY);
 	fd[1] = open("test1.txt", O_RDONLY);
 	fd[2] = open("test2.txt", O_RDONLY);
 	if (fd[0] && fd[1] && fd[2])
